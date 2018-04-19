3293faa27d856f1f3223a7ca81f3e8dafb239a12
Akshay Choudhary <ak92choudhary@gmail.com>
2018-04-19 19:51:40 -0400

    created the UI and The logic for adding books

---
+++ app.js
@@ -0,0 +1,63 @@
+// Book Constructor
+function Book (title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI constructor
+function UI() {
+
+}
+
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+
+    //create tr element
+    const row = document.createElement('tr');
+
+    // insert columns
+    row.innerHTML = `<td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href = "#" class = "delete">X<a></td>`;
+
+    list.appendChild(row);
+}
+
+//clear fields
+UI.prototype.clearfields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+// event listners
+document.getElementById('book-form').addEventListener('submit', function(e){
+
+    //get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+
+          //instantitate book
+
+    const book = new Book(title, author, isbn);
+
+    // instatntiate UI
+
+    const ui = new UI();
+
+
+
+    // add book to list
+
+    ui.addBookToList(book);
+
+//clear fields
+
+    ui.clearfields();
+
+
+    e.preventDefault();
+})
